e6962476727dae1d0725522acbfeeb0336a37454
Ronaldo da Silva <ronaldo.intermec@gmail.com>
2024-12-19 17:51:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+import './index.css'
+export function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    </div>
+
+  )
+}
