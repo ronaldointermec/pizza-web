2222638ac658a5ab1197106d445004dea4afb894
Silva <ronaldo.silva2@honeywell.com>
2024-12-27 15:45:33 -0300

    fixing eslint

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
-    return (
-        <div>
-            <h1>Cabeçalho</h1>
+  return (
+    <div>
+      <h1>Cabeçalho</h1>
 
-            <div>
-                <Outlet />
-            </div>
-        </div>
-    )
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
 }
