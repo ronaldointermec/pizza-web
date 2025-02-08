868a2329f6d2f7c13c05176418ad9976360a53ee
Ronaldo da Silva <ronaldo.intermec@gmail.com>
2025-02-08 15:20:28 -0300

    before orders

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,8 @@
 import { Separator } from '@radix-ui/react-separator'
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { ThemeToggle } from '../theme/theme-toggle'
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
 
 export function Header() {
@@ -19,6 +21,10 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
